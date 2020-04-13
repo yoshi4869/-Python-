b779f155f94bed6d69b9f236ebbfea82fea70c70
yoshi4869 <yoshihito4869@gmail.com>
2020-04-13 22:06:06 +0900

    退屈なことはことはpythonにやらせよう 第五章

---
+++ birthdays.py
@@ -0,0 +1,15 @@
+birthdays = {' アリス': '4/ 1', 'ボブ': '12/ 12', 'キャロル': '4/ 4'} #
+while True: 
+    print(' 名前 を 入力 してください: (終了 するには Enter だけ 押 してください)') 
+    name = input()
+    if name == '':
+         break
+    if name in birthdays: # ❷ 
+        print( name + 'の 誕生日 は' + birthdays[ name]) # ❸ 
+    else:
+        print( name + 'の 誕生日 は 未登録 です。') 
+        print(' 誕生日 を 入力 してください:') 
+        bday = input()
+        birthdays[name] = bday # ❹ 
+        print(' 誕生日 データベース を 更新 しました。')
+
