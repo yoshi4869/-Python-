c61a6481c655f30edc393125c39fa62ebe8e9177
yoshi4869 <yoshihito4869@gmail.com>
2020-06-24 21:39:26 +0900

    メッセージから電話番号を探す

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -16,7 +16,13 @@ def is_phone_number(text):
             return False
     return True #すべて一致すればOK
 
-print('415-555-4242は電話番号：')
-print(is_phone_number('415-555-4242'))
-print('Moshi moshiは電話番号：')
-print(is_phone_number("Moshi moshi"))
+# print('415-555-4242は電話番号：')
+# print(is_phone_number('415-555-4242'))
+# print('Moshi moshiは電話番号：')
+# print(is_phone_number("Moshi moshi"))
+message = '明日415-555-1011に電話してください．オフィスは415-555-9999です．'
+for i in range(len(message)):
+    chunk = message[i:i+12] #
+    if is_phone_number(chunk): #
+        print('電話番号が見つかりました:'+ chunk)
+print('完了')
